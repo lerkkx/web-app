51bb4746cd79c2c18aa6af8273b104520170a8fe
Valeria Sevostyanova <125126489+lerkkx@users.noreply.github.com>
2025-01-03 22:49:36 +0000

    небольшие поправочки

--- web_app/main/views.py
+++ web_app/main/views.py
@@ -1,9 +1,18 @@
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('register/', views.index, name='register'),  # Используем основную страницу для регистрации
-    path('login/', views.user_login, name='user_login')
-]
+def index(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user_login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'main/index.html', {'form': form})
+
+def user_login(request):
+    return render(request, 'main/login.html')
+
+def about(request):
+    return render(request, 'main/about.html')
