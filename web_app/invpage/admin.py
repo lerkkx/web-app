1c1ea52c99df0e27e2abe2bd4bc5f9ae672b5106
Peter <108881076+gl0s3@users.noreply.github.com>
2025-01-13 23:07:45 +0000

    5 6

--- web_app/invpage/admin.py
+++ web_app/invpage/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import InventoryItem, InventoryRequest
+from .models import InventoryItem, InventoryRequest, PurchasePlan, UsageReport
 
 admin.site.register(InventoryItem)
 admin.site.register(InventoryRequest)
+admin.site.register(PurchasePlan)
+admin.site.register(UsageReport)
--- web_app/invpage/models.py
+++ web_app/invpage/models.py
@@ -31,3 +31,19 @@ class Ownership(models.Model):
     def __str__(self):
         return self
 
+class PurchasePlan(models.Model):
+    item = models.ForeignKey('InventoryItem', on_delete=models.CASCADE)
+    planned_price = models.DecimalField(max_digits=10, decimal_places=2)
+    planned_supplier = models.CharField(max_length=255)
+    purchase_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.item.name} - {self.planned_supplier} (Date: {self.purchase_date})"
+
+class UsageReport(models.Model):
+    item = models.ForeignKey('InventoryItem', on_delete=models.CASCADE)
+    notes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Report for {self.item.name} (Created at: {self.created_at})"
